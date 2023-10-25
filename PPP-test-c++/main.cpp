27313d5708381b7b7806135af12589341ef0ec86
“Cyan-iMacPro” <chansonyan@live.com>
2023-10-25 09:39:25 +0800

    Initial Commit

---
+++ PPP-test-c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PPP-test-c++
+//
+//  Created by Cyan on 2023/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
