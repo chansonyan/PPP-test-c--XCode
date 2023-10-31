ae8e77f39d95e9b09888824a0cf6366fca546f82
“Cyan-iMacPro” <chansonyan@live.com>
2023-11-01 00:21:26 +0800

    test 2

---
+++ PPP-test-c++/hello_world.cpp
@@ -0,0 +1,14 @@
+//
+//  hello_world.cpp
+//  PPP-test-c++
+//
+//  Created by Cyan on 2023/11/1.
+//
+
+#include "std_lib_facilities.h"
+int main() // C++ programs start by executing the function main
+{
+    cout << "Hello, World!\n"; // output “Hello, World!”
+    keep_window_open(); // wait for a character to be entered
+    return 0;
+}
